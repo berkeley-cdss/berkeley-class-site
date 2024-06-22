49bcfeff6f235b58236e0e3aa2d21ed29db38299
Rebecca Dang <35876322+phrdang@users.noreply.github.com>
2024-06-21 21:13:49 -0700

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- _includes/test.py
+++ _includes/test.py
@@ -2,13 +2,13 @@ def testing():
     """
     doctests woohoo
     """
-    print('this is displayed')
-    print('this is also displayed')
+    print("this is displayed")
+    print("this is also displayed")
     # BEGIN SOLUTION
-    print('solution is only displayed if enabled')
+    print("solution is only displayed if enabled")
     # END SOLUTION
-    
-    print('this is also displayed')
+
+    print("this is also displayed")
     # BEGIN SOLUTION
-    print('solution 2 is only displayed if enabled')
+    print("solution 2 is only displayed if enabled")
     # END SOLUTION
