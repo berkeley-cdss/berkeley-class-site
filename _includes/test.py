71e2b0cd330f4b65e1a13336763523c4a1e55f7a
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2024-06-21 21:14:23 -0700

    Remove trailing whitespace

--- _includes/test.py
+++ _includes/test.py
@@ -7,7 +7,7 @@ def testing():
     # BEGIN SOLUTION
     print('solution is only displayed if enabled')
     # END SOLUTION
-    
+
     print('this is also displayed')
     # BEGIN SOLUTION
     print('solution 2 is only displayed if enabled')
