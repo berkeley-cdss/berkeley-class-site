c650fd661536b3bd1250620770621f66a8950a12
Brandon Su <brandonsu@berkeley.edu>
2025-11-24 11:48:14 -0800

    code styling updates

--- _plugins/asset_tags.rb
+++ _plugins/asset_tags.rb
@@ -37,13 +37,14 @@ module Jekyll
       parts = input.strip.split(/\s+/, 2)
       @number = parts[0]
       @number_updated = @number.rjust(2, '0')
-    #   @slug = parts[1]
+      # @slug = parts[1]
     end
 
     def render(context)
       lectures = context['site']['lectures']
       lecture = lectures.find { |l| l.basename_without_ext == @number_updated }
-      return "No lecture found" unless lecture # in case the lecture isn't found
+      return 'No lecture found' unless lecture # in case the lecture isn't found
+
       lecture_title = lecture['title']
       lecture_url = lecture.url
       # Directly grab the date string from the lecture data
@@ -51,25 +52,25 @@ module Jekyll
       year = date_parts[0].to_i
       month = date_parts[1].to_i
       day = date_parts[2].to_i
-      if !lecture['time'].nil?
-        hour = lecture['time'] / 60 # lecture['time'] is expressed in minutes (i.e. 10:40 is 640)
-        minute = lecture['time'] % 60
-      else
+      if lecture['time'].nil?
         hour = 0
         minute = 0
-    end
+      else
+        hour = lecture['time'] / 60 # lecture['time'] is expressed in minutes (i.e. 10:40 is 640)
+        minute = lecture['time'] % 60
+      end
       # Construct the date manually
       lecture_time = Time.new(year, month, day, hour, minute)
-  Jekyll.logger.info "DEBUG", "Time is #{lecture_time}"
+      Jekyll.logger.info "DEBUG", "Time is #{lecture_time}"
 
-  current_time = Time.now
-  Jekyll.logger.info "DEBUG", "Current Time is #{current_time}"
+      current_time = Time.now
+      Jekyll.logger.info "DEBUG", "Current Time is #{current_time}"
 
-  # Always render an anchor so client-side JS can toggle links in real time.
-  # Attach a machine-readable ISO timestamp in `data-release`.
-  release_iso = lecture_time.utc.iso8601
-  # Use the lecture's URL (Jekyll document.url) and include the data-release attribute.
-  "**Lecture #{@number}**{: .label .label-lec } <a href=\"..#{lecture_url}\" data-release=\"#{release_iso}\">#{lecture_title}</a>"
+      # Always render an anchor so client-side JS can toggle links in real time.
+      # Attach a machine-readable ISO timestamp in `data-release`.
+      release_iso = lecture_time.utc.iso8601
+      # Use the lecture's URL (Jekyll document.url) and include the data-release attribute.
+      "**Lecture #{@number}**{: .label .label-lec } <a href=\"..#{lecture_url}\" data-release=\"#{release_iso}\">#{lecture_title}</a>"
       
     end
   end
